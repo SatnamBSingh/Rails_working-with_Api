00798d5bc515e52b35b27a56232b9506fc3d80b2
SatnamSingh <satnam.ss940@gmail.com>
2020-06-03 18:05:17 +0530

    initial commit api set

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Article.create(title: 'Hello world', content: 'lorem ipso', slug: 'hello-world') 
+Article.create(title: 'Hello World', content: 'test', slug: 'hello-worldkcvk')
+Article.create(title: 'test', content: 'testing', slug: 'this-is-test-article')
+Article.create(title: 'Example', content: 'exmpleblog', slug: 'One of my favourite type of Blog is the “tech blog”.  Now this isn’t something new to blog about, as Tech bloggers have been blogging about technology news and gadget reviews in detail online since the beginning of the internet, But because it’s such a vast niche, you could carve out yourself a really good angle within the tech blog sphere.')
